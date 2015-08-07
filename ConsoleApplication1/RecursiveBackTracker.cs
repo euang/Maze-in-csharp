85013585e1f885ca82f83d11b71da8b390799dc3
euan <euan_griffiths@btinernet.com>
2015-08-07 15:30:05 +0100

    added recursive backtracker

---
+++ ConsoleApplication1/RecursiveBackTracker.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    internal class RecursiveBackTracker
+    {
+        private Random rand = new Random();
+
+        public Grid On(Grid grid,Cell startAt = null)
+        {
+            if (startAt == null)
+            {
+                startAt = grid.RandomCell();
+            }
+
+            var stack = new Stack<Cell>() ;
+            stack.Push(startAt);
+
+            while (stack.Count != 0)
+            {
+                var current = stack.Peek();
+                var neighbours = current.Neighbours().Where(n => n.Links.Count == 0).ToList();
+                if (neighbours.Count == 0)
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    var neighbour = neighbours[rand.Next(neighbours.Count)];
+                    current.Link(neighbour);
+                    stack.Push(neighbour);
+                }
+
+
+            }
+
+            return grid;
+        }
+    }
+}
